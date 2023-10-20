d7bc9dcbb6f17796306b428bdab36010c01b4e8a
Airton <airtondeodato2007@hotmail.com>
2023-10-20 12:48:24 -0300

    envio de arquivos para hospedagem servidor linux

--- Agenda/frontend/main.js
+++ Agenda/frontend/main.js
@@ -2,5 +2,12 @@ import 'core-js/stable';
 
 import 'regenerator-runtime/runtime';
 
-import './assets/css/style.css';
+import Login from './modules/Login'
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+
+login.init();
+cadastro.init();
+// import './assets/css/style.css';
 
