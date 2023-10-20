193f0b8fa230172117536fb2e7c38be4237d2fc6
Airton <airtondeodato2007@hotmail.com>
2023-10-20 14:59:42 -0300

    remoção do helmet para solucionar problema de redirecionamento nginx

--- Agenda/server.js
+++ Agenda/server.js
@@ -23,8 +23,8 @@ const {
   csrfMiddleware,
 } = require("./src/middlewares/middeware");
 
-app.use(helmet());
-app.use(helmet.referrerPolicy({ policy: ["origin", "unsafe-url"] }));
+// app.use(helmet());
+// app.use(helmet.referrerPolicy({ policy: ["origin", "unsafe-url"] }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
