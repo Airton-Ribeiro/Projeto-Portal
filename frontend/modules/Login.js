39fd637da619c563d0c50809b037b4692d117451
IgorVianadF <igorv02004@gmail.com>
2023-11-07 22:14:55 -0300

    commit inicial

--- frontend/modules/Login.js
+++ frontend/modules/Login.js
@@ -5,30 +5,30 @@ export default class login {
         this.form = document.querySelector(formClass);
 
     }
-    init(){
+    init() {
         this.events();
     }
-    events(){
-        if(!this.form) return;
+    events() {
+        if (!this.form) return;
         this.form.addEventListener('submit', e => {
             e.preventDefault();
             alert('FORM NÃO ENVIADO');
             this.validate(e)
         })
     }
-    validate(e){
+    validate(e) {
         const el = e.target;
         const emailInput = el.querySelector('input[name="email"]');
         const passwordInput = el.querySelector('input[name="password"]');
-        let error =  false;
-        if(!validator.isEmail(emailInput.value)){
+        let error = false;
+        if (!validator.isEmail(emailInput.value)) {
             alert('Email invalido');
             error = true;
         }
-        if(passwordInput.value.length < 3 || passwordInput.value.length > 50){
+        if (passwordInput.value.length < 3 || passwordInput.value.length > 50) {
             alert('Senha precisa ter entre 3 e 50 caracteres');
             error = true;
         }
-        if(!error) el.submit();
+        if (!error) el.submit();
     }
 }
