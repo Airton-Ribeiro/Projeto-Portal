9dda8e6aeba4e777264e2b9c1b19559565190e91
moonlit <moonlit@debian>
2023-11-14 23:32:45 -0300

    melhoria estética da listagem, inseridos numeros para identificar cada aluno e consertada a ordem de aparecimento do novo aluno (fim da lista)

--- routes.js
+++ routes.js
@@ -1,35 +1,33 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
 
+const homeController = require("./src/controllers/homeController");
+const loginController = require("./src/controllers/loginController");
+const contatoController = require("./src/controllers/contatoController");
 
-const homeController = require('./src/controllers/homeController');
-const loginController = require('./src/controllers/loginController');
-const contatoController = require('./src/controllers/contatoController');
-
-const { loginRequired } = require('./src/middlewares/middeware');
+const { loginRequired } = require("./src/middlewares/middeware");
 
 // Rotas da home
-route.get('/', homeController.index);
+route.get("/", homeController.index);
 
 // Rotas de login
 
-route.get('/login/index', loginController.index);
-route.post('/login/login', loginController.login);
-route.get('/login/logout', loginController.logout);
-route.get('/login/login/logout', loginController.logout);
-route.post('/aluno/enviar', loginController.enviar);
-route.post('/aluno/checar', loginController.checar);
-route.post('/login/professor', loginController.loginProfessor);
-route.post('/login/professor/login', loginController.login);
+route.get("/login/index", loginController.index);
+route.post("/login/login", loginController.login);
+route.get("/login/logout", loginController.logout);
+route.get("/login/login/logout", loginController.logout);
+route.post("/aluno/enviar", loginController.enviar);
+route.post("/aluno/checar", loginController.checar);
+route.post("/login/professor", loginController.loginProfessor);
+route.post("/login/professor/login", loginController.login);
 // Rotas de contato
 
-route.get('/contato/index', loginRequired, contatoController.index);
-route.post('/contato/register', loginRequired, contatoController.register);
-route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
-route.post('/contato/edit/:id', loginRequired, contatoController.edit);
-route.get('/contato/delete/:id', loginRequired, contatoController.delete);
-route.get('/contato/login/logout', loginController.logout);
-route.get('/contato/atividade/:id', loginRequired, contatoController.avaliar);
-
+route.get("/contato/index", loginRequired, contatoController.index);
+route.post("/contato/register", loginRequired, contatoController.register);
+route.get("/contato/index/:id", loginRequired, contatoController.editIndex);
+route.post("/contato/edit/:id", loginRequired, contatoController.edit);
+route.get("/contato/delete/:id", loginRequired, contatoController.delete);
+route.get("/contato/login/logout", loginController.logout);
+route.get("/contato/atividade/:id", loginRequired, contatoController.avaliar);
 
 module.exports = route;
