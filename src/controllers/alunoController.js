25ad231fdffc18a49c1d576a756d5f904fced176
Júlio César <juliocfds20@gmail.com>
2023-11-21 12:57:21 -0300

    ajustado o front da tela de aceitar ativides

--- src/controllers/alunoController.js
+++ src/controllers/alunoController.js
@@ -80,7 +80,7 @@ exports.avaliar = async (req, res) => {
 
 
 //Tentativa de download de arquivo CTRL+click no "uploadFile" de baixo pra ir para a função no AlunoModel
-exports.uploadFile = async (req, res) => {
-  const aluno = new Aluno(req.body);
-  await aluno.uploadFile(req, res);
-}
+// exports.uploadFile = async (req, res) => {
+//   const aluno = new Aluno(req.body);
+//   await aluno.uploadFile(req, res);
+// }
